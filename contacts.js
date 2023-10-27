b7e8d2b9831f7a3b72d499de53007783d1482e63
Pavel <87939143+PavelZavadskiy@users.noreply.github.com>
2023-10-27 17:35:06 +0300

    Updated after wathed lections

--- contacts.js
+++ contacts.js
@@ -2,61 +2,38 @@ const fs = require('fs').promises;
 const { nanoid } = require('nanoid');
 const path = require('path');
 
-const contactsPath = path.join('db', 'contacts.json');
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
 async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath, 'utf8', (err, _) => {
-      if (err) throw err;
-    });
-    return JSON.parse(data);
-  } catch (error) {
-    throw error;
-  }
+  const data = await fs.readFile(contactsPath, 'utf8');
+  return JSON.parse(data);
 }
 
 async function getContactById(contactId) {
-  try {
-    const array = await listContacts();
-    if (array.length === 0) return null;
-    const contact = array.find(item => item.id === contactId);
-    if (contact === undefined) return null;
-    return contact;
-  } catch (error) {
-    throw error;
-  }
+  const array = await listContacts();
+  if (array.length === 0) return null;
+  const contact = array.find(item => item.id === contactId);
+  return contact || null;
 }
 
 async function removeContact(contactId) {
-  try {
-    const array = await listContacts();
-    if (array.length === 0) return null;
-    const contact = array.find(item => item.id === contactId);
-    if (contact === undefined) return null;
-    const newArray = array.filter(item => item.id !== contactId);
-    fs.writeFile(contactsPath, JSON.stringify(newArray), 'utf8', err => {
-      if (err) throw err;
-    });
-    return contact;
-  } catch (error) {
-    throw error;
-  }
+  const array = await listContacts();
+  if (array.length === 0) return null;
+  const contact = array.find(item => item.id === contactId);
+  if (contact === undefined) return null;
+  const newArray = array.filter(item => item.id !== contactId);
+  fs.writeFile(contactsPath, JSON.stringify(newArray, null, 2), 'utf8');
+  return contact;
 }
 
 async function addContact(name, email, phone) {
-  try {
-    const array = await listContacts();
-    let contact = array.find(item => item.email === email || name.email === name);
-    if (contact !== undefined) throw new Error('CONTACT_EXISTS');
-    contact = { id: nanoid(), name, email, phone };
-    array.push(contact);
-    fs.writeFile(contactsPath, JSON.stringify(array), 'utf8', err => {
-      if (err) throw err;
-    });
-    return contact;
-  } catch (error) {
-    throw error;
-  }
+  const array = await listContacts();
+  let contact = array.find(item => item.email === email || name.email === name);
+  if (contact !== undefined) throw new Error('CONTACT_EXISTS');
+  contact = { id: nanoid(), name, email, phone };
+  array.push(contact);
+  fs.writeFile(contactsPath, JSON.stringify(array, null, 2), 'utf8');
+  return contact;
 }
 
 module.exports = {
